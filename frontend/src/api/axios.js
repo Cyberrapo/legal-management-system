71d270674bb793914d36b8806af9f205a7f0bd27
Vaishak Poojary <vaishakpoojary58@gmail.com>
2026-03-26 04:04:18 +0530

    fix backend url

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API = axios.create({
-  baseURL: 'https://legal-backend-ustd.onrender.com'
+  baseURL: 'https://legal-backend-ustd.onrender.com/api'
 })
 
 API.interceptors.request.use((req) => {
