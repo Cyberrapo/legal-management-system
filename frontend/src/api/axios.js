ae1a50764513b3171fdec7fd2bcf7a9227adf251
Vaishak Poojary <vaishakpoojary58@gmail.com>
2026-03-25 15:26:12 +0530

    update api url to production

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 
-const API = axios.create({ baseURL: 'http://localhost:5000/api' })
+const API = axios.create({
+  baseURL: 'https://legal-backend-ustd.onrender.com'
+})
 
 API.interceptors.request.use((req) => {
   const user = JSON.parse(localStorage.getItem('user'))
